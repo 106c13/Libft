4294732a9884876ee60d5a754d17bb971cb2341f
106c13 <terbclub@gmail.com>
2025-02-01 00:06:16 -0800

    Update ft_print.c

--- ft_print.c
+++ ft_print.c
@@ -3,40 +3,61 @@
 #include <stdarg.h>
 #include "tmp.h"
 
+static char	*ft_ltoa(char type, void *num)
 
-static char	*convert_to_string(char c, )
+
+static void	ft_printstr(char *str)
 {
-
-	if (c == 'd')
-		return (ft_i)
-
-}
-
-
-int	ft_print(const char *input, ...)
-{
-	va_list args;  
-	va_start(args, input);  
-	int		i;
-	int 	total_printed;
-	char	*str;
-
-	i = va_arg(args, int);
-	total_printed = 0;
-	while (*input)
+	while (*str)
 	{
-		if (*input == '%')
-			str = convert_to_string(*(input + 1), )
+		write(1, str, 1);
+		str++;
 	}
-	va_end(args);
-	return (0);
 }
 
+static char *convert_to_string(char type, va_list *args) {
+    if (type == 'd') 
+        return ft_itoa(va_arg(*args, int));
+    else if (type == 'c')
+    {
+        char *result = (char *)malloc(2 * sizeof(char));
+        result[0] = (char)va_arg(*args, int);
+        result[1] = '\0';
+        return (result);
+    }
+    else if (type == 's')
+    	return (va_arg(*args, char *));
+    else if (type == 'lld')
+    	return (ft_ltoa(type, ))
 
-int	main()
-{
-	char	*s;
-	char	*str = "0123456789";
-	ft_print("%d %d %d", 2, 3, 0);
-	return 0;
+    return NULL;
+}
+
+int ft_printf(const char *input, ...) {
+    va_list args;
+    va_start(args, input);
+    char *str;
+
+    while (*input) {
+        if (*input == '%')
+        {
+            str = convert_to_string(*(input + 1), &args);
+            if (str != NULL)
+            {
+                ft_printstr(str);
+                free(str);
+            }
+            input++;  // Skip over the format specifier
+        }
+        else
+            write(1, input, 1);
+        input++;
+    }
+    va_end(args);
+    return 0;
+}
+
+int main() {
+    ft_printf("%lld", 9223372036854775807);
+    return 0;
 }
